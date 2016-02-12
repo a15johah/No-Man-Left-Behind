2a2e93ea3b54053581217bd5dcb9ceabb0acbe7a
a15johah <a15johah@student.his.se>
2016-02-12 13:02:42 +0100

    Injured
    Functional Injured class

--- Core/Injured.h
+++ Core/Injured.h
@@ -1,7 +1,34 @@
 #pragma once
 
-class Injured{
+#include <vector>
+#include <map>
+
+#include "Collidable.h"
+#include "Item.h"
+
+static enum Injure{
+	DEAD = 0,
+	GRAVE = 1,
+	SEVERE = 2,
+	LIGHT = 3,
+	HEALED = 4
+};
+
+class Injured : public Collidable{
 public:
 	Injured();
 	~Injured();
+
+	const Injure nextStage(const Injure& current);
+	const Injure previusStage(const Injure& current);
+
+	const Injure parseInjure(const std::string& s);
+	const std::string injureToString(const Injure& Injure);
+
+	std::vector<Resource>* getRequirements(const Injure inj);
+	void setRequirements(Injure inj, std::vector<Resource>* req);
+
+	void clear();
+private:
+	std::map<Injure, std::vector<Resource>*> requirements;
 };
